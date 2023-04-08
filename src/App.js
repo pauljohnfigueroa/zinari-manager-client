ae6e2a74cf8044dc0090af335f12a0d3624db1df
Paul Figueroa <pjsfigueroa@gmail.com>
2023-04-08 12:48:38 +0800

    Prep the routes and login page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useMemo } from 'react'
+import { BrowserRouter, Routes, Navigate, Route } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from 'theme'
+
+import LoginPage from 'pages/login/LoginPage'
 
 function App() {
+
+  // get the theme mode from the global state
+  const mode = useSelector(state => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode), [mode]))
+
+  const isAuth = Boolean(useSelector(state => state.token))
+
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/state/redux.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Global app state
+const initialState = {
+    mode: "dark",
+    user: null,
+    token: null,
+    tasks: []
+}
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    // reducer are functions that modify the global state
+    reducers: {
+        // change theme to light or dark mode
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light"
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user
+            state.token = action.payload.token
+        },
+        setLogout: (state) => {
+            state.user = null
+            state.token = null
+        },
+        setFriends: (state, action) => {
+            if (state.user) {
+                state.user.friends = action.payload.friends
+            } else {
+                console.error("User's friends are non-existent.")
+            }
+        },
+        setTasks: (state, action) => {
+            state.tasks = action.payload.tasks
+        },
+        setTask: (state, action) => {
+            const updatedPosts = state.tasks.map(task => {
+                if (task._id === action.payload.task._id) return action.payload.task
+
+                return task
+            })
+            state.tasks = updatedTasks
+        }
+    }
+
+})
+
+export const { setMode, setLogin, setLogout, setFriends, setTasks, setTask } = authSlice.actions
+export default authSlice.reducer
